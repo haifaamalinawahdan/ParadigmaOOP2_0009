4a4b325632e8356814d285b651485d2d6023ff33
haifaamalinawahdan <haifanew3@gmail.com>
2026-05-18 10:45:18 +0700

    membuat  class seseorang dengan virtual function

---
+++ polimorpi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang{
+public:
+    virtual void pesan() = 0;
+    // virtual void(){
+    //      cout << "Pesan dari seseorang" << endl;
+    // }
+};
+
